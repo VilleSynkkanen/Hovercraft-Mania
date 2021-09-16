57f0ebf3400da242e2dc11ae068b10421ba6624b
Ville Synkkänen <ville.synkkanen@gmail.com>
2021-09-16 20:50:31 +0300

    Track map

---
+++ Assets/Scripts/HovercraftHud.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class HovercraftHud : MonoBehaviour
+{
+    [SerializeField] RectTransform speedoNeedle;
+    [SerializeField] TextMeshProUGUI speedText;
+    [SerializeField] float needleRotationMultiplier;
+    [SerializeField] float maxNeedleRotationPerSecond;
+    [SerializeField] RectTransform playerBlip;
+    [SerializeField] Transform trackCenter;
+    [SerializeField] float trackMapPosMultiplier;
+
+    HovercraftMovement movement;
+    float speed;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        movement = GetComponent<HovercraftMovement>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float newSpeed = movement.rb.velocity.magnitude;
+
+        if(Mathf.Abs(newSpeed - speed) > maxNeedleRotationPerSecond * Time.deltaTime)
+        {
+            if(newSpeed < speed)
+            {
+                speed -= maxNeedleRotationPerSecond * Time.deltaTime;
+            }
+            else
+            {
+                speed += maxNeedleRotationPerSecond * Time.deltaTime;
+            }
+        }
+
+        speedoNeedle.rotation = Quaternion.Euler(0, 0, needleRotationMultiplier * speed);
+        speedText.text = ((int)(newSpeed * 3.6f)).ToString();
+
+        Vector3 playerPos = movement.transform.position - trackCenter.position;
+        playerBlip.anchoredPosition = trackMapPosMultiplier * new Vector3(playerPos.x, playerPos.z, 0);
+    }
+}
